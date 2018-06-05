401aa139e65ea87001d50f9532168993f985f428
Bojan Todorovic <c.btodorovic@gogoair.com>
2018-06-05 20:40:15 +0200

    implemented screenshot testing

--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -4,58 +4,63 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 import { login } from '../../Redux/actions/auth/login';
-import { clearFormStatus } from "../../Redux/actions/clearFormStatus";
+import { clearFormStatus } from '../../Redux/actions/clearFormStatus';
 import UserForm from './UserForm';
 
-class _Login extends React.Component {
-    static propTypes = {
-        login: PropTypes.func,
-        errorMessage: PropTypes.any,
-        clearFormStatus: PropTypes.func,
-        isLoading: PropTypes.bool,
-        history: PropTypes.object,
-        location: PropTypes.object,
-    };
+export class _Login extends React.Component {
+	static propTypes = {
+		login: PropTypes.func,
+		errorMessage: PropTypes.any,
+		clearFormStatus: PropTypes.func,
+		isLoading: PropTypes.bool,
+		history: PropTypes.object,
+		location: PropTypes.object,
+	};
 
-    componentDidMount() {
-        this.accountActivated = this.props.location.search.split('=')[0] === '?accActivated'
-            && !!this.props.location.search.split('=')[1];
-    }
+	static defaultProps = {
+		login: () => {},
+		clearFormStatus: () => {},
+		history: {},
+		location: { search: '' },
+	};
 
-    redirectToHome = () => {
-        this.props.history.replace('/');
-    };
+	redirectToHome = () => {
+		this.props.history.replace('/');
+	};
 
-    render() {
-        return (
-            <UserForm
-                handleSubmit={this.props.login.bind(null, this.redirectToHome)}
-                formMode="login"
-                formError={this.props.errorMessage}
-                clearFormStatus={this.props.clearFormStatus}
-                isLoading={this.props.isLoading}
-                accountActivated={this.accountActivated}
-            />
-        );
-    }
+	render() {
+		const accountActivated =
+			this.props.location.search.split('=')[0] === '?accActivated' && !!this.props.location.search.split('=')[1];
+
+		return (
+			<UserForm
+				handleSubmit={this.props.login.bind(null, this.redirectToHome)}
+				formMode="login"
+				formError={this.props.errorMessage}
+				clearFormStatus={this.props.clearFormStatus}
+				isLoading={this.props.isLoading}
+				accountActivated={accountActivated}
+			/>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-    return {
-        errorMessage: state.loginStatus.errorMessage,
-        isLoading: state.loginStatus.isLoading,
-    };
+	return {
+		errorMessage: state.loginStatus.errorMessage,
+		isLoading: state.loginStatus.isLoading,
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        login: (redirectCallback, params) => {
-            return dispatch(login(redirectCallback, params));
-        },
-        clearFormStatus: () => {
-            return dispatch(clearFormStatus());
-        }
-    };
+	return {
+		login: (redirectCallback, params) => {
+			return dispatch(login(redirectCallback, params));
+		},
+		clearFormStatus: () => {
+			return dispatch(clearFormStatus());
+		},
+	};
 };
 
 const Login = connect(mapStateToProps, mapDispatchToProps)(_Login);
